1db841309fa31be1940059a2e82f67ed88b09274
Aniket Keshri <60981763+SubZero-13@users.noreply.github.com>
2022-04-20 02:24:16 +0530

    Add files via upload

---
+++ Missing number in array.java
@@ -0,0 +1,11 @@
+class Solution {
+    int MissingNumber(int array[], int n) {
+        // Your Code Here
+        int sum = 0;
+        for(int i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        int orgSum = ((n*(n+1))/ 2);
+        return orgSum - sum;
+    }
+}
